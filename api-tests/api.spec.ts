d6ba1420e1626d02f92a478f3147c029fac90f73
Alexander Ponomarenko <brain16383@yandex.ru>
2021-07-17 01:44:05 +0300

    added dependencies

---
+++ api-tests/api.spec.ts
@@ -0,0 +1,14 @@
+import { expect } from '@jest/globals';
+
+interface User {
+    name: string;
+    role: string;
+    id: number;
+}
+describe('Courses', () => {
+
+    it('gets a user by id', async () => {
+        expect(true).toBe(true);
+    });
+
+});
