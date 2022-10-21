014fdb13a95abc7e15f09dae97cda52a68473559
Alexander Ponomarenko <brain16383@yandex.ru>
2022-10-22 01:33:50 +0300

    [AsmApi] change .sh

--- apiClient/1.sh
+++ apiClient/1.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-npm run wordStart
-npm run docOpen "d:/www/doc1.doc"
-npm run wordClose
+# npm run wordStart
+# npm run docOpen "d:/www/doc1.doc"
+# npm run wordClose
 
+./asm.sh wordStart
+./asm.sh docOpen "d:/www/doc1.doc"
+./asm.sh wordClose
---
+++ apiClient/asm.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+ts-node --project tsconfig.json -r tsconfig-paths/register app.ts $1 $2 $3
